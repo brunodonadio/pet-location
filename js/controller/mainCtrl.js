b99aaa1175dd93edf4e11b97f9205c4f6b1f8168
Bruno Donadío <bruno.donadio@icloud.com>
2017-03-06 01:13:51 -0300

    Función para obtener una posición aleatoria según la posición actual en el mapa.

--- js/controller/mainCtrl.js
+++ js/controller/mainCtrl.js
@@ -1,5 +1,9 @@
 angular.module('petLocationApp', ['ngMap', 'ngFileUpload'])
     .controller('MainController', ['$scope', 'NgMap', function($scope, NgMap) {
+        NgMap.getMap('map').then(function(map) {
+            $scope.map = map;
+        });
+
         $scope.pets = [];
 
         $scope.addPet = function (name, plight, photo) {
@@ -15,5 +19,18 @@ angular.module('petLocationApp', ['ngMap', 'ngFileUpload'])
             };
 
             $scope.pets.push(newPet);
+        };
+
+        $scope.getRandomPosition = function (map) {
+            var center = map.getCenter();
+
+            var position;
+            do {
+                var latDeviation = (Math.random() * 0.031653) * (Math.random() < 0.5 ? -1 : 1);
+                var lngDeviation = (Math.random() * 0.031653) * (Math.random() < 0.5 ? -1 : 1);
+                position = new google.maps.LatLng(center.lat() + latDeviation, center.lng() + lngDeviation);
+            } while (google.maps.geometry.spherical.computeDistanceBetween(center, position) > 3000);
+
+            return position;
         };
     }]);
