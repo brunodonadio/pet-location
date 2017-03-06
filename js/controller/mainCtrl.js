13f3fcb5c42cde9be4692b3c14cb3414e5ca04df
Bruno Donadío <bruno.donadio@icloud.com>
2017-03-06 01:09:44 -0300

    Función para añadir nuevas mascotas, con su correspondiente formulario.

--- js/controller/mainCtrl.js
+++ js/controller/mainCtrl.js
@@ -1,4 +1,19 @@
-angular
-    .module('petLocationApp', ['ngMap', 'ngFileUpload'])
-        .controller('MainController', ['$scope', 'NgMap', function($scope, NgMap) {
-}]);
+angular.module('petLocationApp', ['ngMap', 'ngFileUpload'])
+    .controller('MainController', ['$scope', 'NgMap', function($scope, NgMap) {
+        $scope.pets = [];
+
+        $scope.addPet = function (name, plight, photo) {
+            var position = $scope.getRandomPosition($scope.map);
+            var newPet = {
+                name: name,
+                plight: plight,
+                photo: photo,
+                marker: {
+                    lat: position.lat(),
+                    lng: position.lng()
+                }
+            };
+
+            $scope.pets.push(newPet);
+        };
+    }]);
